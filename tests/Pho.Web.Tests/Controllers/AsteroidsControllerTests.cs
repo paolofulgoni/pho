b8df84691926714dc944af7d4ae96f7073e5456f
Paolo Fulgoni <paolo.fulgoni@truelayer.com>
2023-01-29 14:07:56 +0100

    Add test project

---
+++ tests/Pho.Web.Tests/Controllers/AsteroidsControllerTests.cs
@@ -0,0 +1,16 @@
+namespace Pho.Web.Tests;
+
+public class AsteroidsControllerTests
+{
+    [Fact]
+    public void SampleOkTest()
+    {
+        Assert.Equal(1, 1);
+    }
+    
+    [Fact]
+    public void SampleNotOkTest()
+    {
+        Assert.Equal(1, 2);
+    }
+}
